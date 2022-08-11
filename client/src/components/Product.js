000be6a1d514d6a1aeac0362efe101ccb1297c07
Labheshwar Sharma <labeshsharma2001@gmail.com>
2022-08-11 14:18:05 +0500

    Updated Products

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -6,9 +6,14 @@ import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='card my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} alt={product.name} variant='top' />
+        <Card.Img
+          className='product-img'
+          src={product.image}
+          alt={product.name}
+          variant='top'
+        />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`} className='link'>
@@ -16,14 +21,15 @@ const Product = ({ product }) => {
             <strong className='theme-color'>{product.name}</strong>
           </Card.Title>
         </Link>
-
-        <Card.Text as='div'>
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <div className='rating-price-container'>
+          <Card.Text as='div'>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </Card.Text>
+          <Card.Text as='h3'>${product.price}</Card.Text>
+        </div>
       </Card.Body>
     </Card>
   );
